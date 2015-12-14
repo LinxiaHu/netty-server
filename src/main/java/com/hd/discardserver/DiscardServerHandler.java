3908d3398056e88c2d1e65e5889ed37245928f15
LinxiaHu <chunyun@mail.ustc.edu.cn>
2015-12-14 15:13:43 +0800

    change discart to print msg

--- src/main/java/com/hd/discardserver/DiscardServerHandler.java
+++ src/main/java/com/hd/discardserver/DiscardServerHandler.java
@@ -4,13 +4,23 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
 
 public class DiscardServerHandler extends ChannelHandlerAdapter implements
 		ChannelHandler {
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg)
 			throws Exception {
-		((ByteBuf)msg).release();
+//		((ByteBuf)msg).release();
+		try {
+			ByteBuf in = (ByteBuf)msg;
+			while(in.isReadable()) {
+				System.out.println((char)in.readByte());
+				System.out.flush();
+			}
+		} finally {
+			ReferenceCountUtil.release(msg);
+		}
 	}
 
 	@Override
